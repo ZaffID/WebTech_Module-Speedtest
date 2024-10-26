38761d3b8e5fb96ec5efbb1719c3a258839e3b39
Dimass <155595053+ZaffID@users.noreply.github.com>
2024-10-26 14:28:07 +0700

    Soal A1

---
+++ assets/js/jsnya.js
@@ -0,0 +1,9 @@
+// Fungsi untuk membuka halaman chat
+function openChat() {
+    window.location.href = 'chat.html';
+}
+
+// Fungsi untuk kembali ke halaman kontak
+function goBack() {
+    window.location.href = 'index.html';
+}
